917c330dc8dd912078555c6ab438b6c4d34b25f7
Esteban Villacis <esteban.andres.villacis@gmail.com>
2014-08-24 23:26:28 -0500

    Por corregir Contacto OLA y sus partes

--- application/classes/Model/ContactoOla.php
+++ application/classes/Model/ContactoOla.php
@@ -12,7 +12,7 @@ class Model_ContactoOla extends ORM {
     public $_table_name="contactoOla";
     
     protected $_has_many = array('infocontactosola' => array('model' => 'InformacionContactoOla', 'foreign_key' => 'ContactoOLA_idContactoOLA'));
-    protected $_belongs_to = array('cliente' => array('model' => 'Cliente', 'foreign_key' => 'Cliente_idCliente'));   
+    protected $_belongs_to = array('ola' => array('model' => 'Ola', 'foreign_key' => 'ContactoOLA_idContactoOLA'));   
 }
 
 ?>
