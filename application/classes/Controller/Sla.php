9bfa073627401249ffaa7362015fdf9e59ca6df5
Santiago García <santiagogarciabautista@gmail.com>
2014-09-02 23:21:13 -0500

    Index SLA

--- application/classes/Controller/Sla.php
+++ application/classes/Controller/Sla.php
@@ -51,53 +51,10 @@ class Controller_Sla extends Controller_Containers_Default {
        
         $this->view->set("sla", $sla);
         $this->view->set("opCriticidad", $sla);
-	}
-        
-        public function action_edit()
-	{
-	$this->view = View::factory('ola/edit');
-
-        $ola = ORM::factory('Ola', $this->params[0]);
-        
-       
-        if (!empty($_POST)) {
-                  
-            $db = Database::instance();
-            $db ->begin();
-            try {
-                if ($ola->id) {
-                    $success_message = Kohana::message('sab', 'ola:update:success');
-                } else {
-                    $success_message = Kohana::message('sab', 'ola:create:success');
-                }
-                
-                $ola->criticidad = arr::get($this->request->post(), 'criticidad');
-                $ola->tiempoRespuesta = arr::get($this->request->post(), 'tiempoRespuesta');
-                $ola->medTiempo = arr::get($this->request->post(), 'medTiempo');
-                $ola->descripcion = arr::get($this->request->post(), 'descripcion');
-                $ola->ServicioProveedor_idServicioProveedor = arr::get($this->request->post(), 'proveedorh');
-             
-                $ola->save();                
-                
-                $db->commit();                
-                
-                FlashMessenger::factory()->set_message('success', $success_message);
-                HTTP::redirect('ola/index');
-                
-            } catch (Database_Exception $ex) {
-                foreach ($ex->errors('validation') as $error) {
-                    FlashMessenger::factory()->set_message('error', $error);
-                }
-                 $db->rollback();
-            }
-        }
-        
-        $this->view->set('ola', $ola);
-        $this->view->set("opCriticidad", $ola); 
-        
-        }
-        
-        public function action_loadola() {
+    }
+    
+    
+    public function action_loadsla() {
         if ($this->request->is_ajax()) {
             
             $id = arr::get($this->request->post(), 'id');
@@ -116,51 +73,19 @@ class Controller_Sla extends Controller_Containers_Default {
             $tiempoRespuesta = str_replace($convert_from, $convert_to, $tiempoRespuesta);
 
          
-            $ola = ORM::factory('Ola')                    
+            $sla = ORM::factory('Sla')                    
                     ->where(DB::expr("LOWER(criticidad)"), 'LIKE', DB::expr("_utf8 '%" . $criticidad . "%' collate utf8_bin"))
                     ->and_where(DB::expr("LOWER(tiempoRespuesta)"), 'LIKE', DB::expr("_utf8 '%" . $tiempoRespuesta . "%' collate utf8_bin"))
                     ->and_where(DB::expr("LOWER(medTiempo)"), 'LIKE', DB::expr("_utf8 '%" . $medTiempo . "%' collate utf8_bin"))
                     ->find_all();
 
-            $this->view = View::factory('ola/loads/loadola');
-            $this->view->set('ola', $ola);
+            $this->view = View::factory('sla/loads/loadsla');
+            $this->view->set('sla', $sla);
            
             $this->auto_render = FALSE;
             echo $this->view;
         }
     }
-    
-    public function action_autocompleterproveedor() {
-        if ($this->request->is_ajax()) {
-            $data = arr::get($this->request->query(), 'q');
-
-            //GET A LIST OF SUPPLIER TO LOAD THE AUTOCOMPLETER
-
-            $convert_to = array(
-                "a", "e", "i", "o", "u", "a", "e", "i", "o", "u", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
-            );
-            $convert_from = array(
-                "á", "é", "í", "ó", "ú", "Á", "É", "Í", "Ó", "Ú", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-            );
-            $data = str_replace($convert_from, $convert_to, $data);
-
-
-            $proveedores = ORM::factory('servicioproveedor')
-                    ->where(DB::expr("LOWER(id)"), 'LIKE', DB::expr("_utf8 '%" . $data . "%' collate utf8_bin"))                    
-                    ->or_where(DB::expr("LOWER(nombre)"), 'LIKE', DB::expr("_utf8 '%" . $data . "%' collate utf8_bin"))
-                    ->or_where(DB::expr("LOWER(descripcion)"), 'LIKE', DB::expr("_utf8 '%" . $data . "%' collate utf8_bin"))
-                    ->find_all();
-
-            if ($proveedores->count()) {
-                foreach ($proveedores as $proveedor) {
-                    echo $proveedor->id . '|' . $proveedor->nombre . '|' . $proveedor->descripcion . "\n";
-                }
-            } else {
-                echo '0' . "\n";
-            }
-            $this->auto_render = FALSE;
-        }
-             
-    }
+        
 
 } 
